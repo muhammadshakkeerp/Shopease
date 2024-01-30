06be1b50aa2ce422e732682432e2bb52ce28ae10
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-01-30 18:10:45 +0530

    Removed jest test files & packages to fix errors - deleted src/firebase/firebse.tsx - modified package.json , tsconfig.json , main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store, { client } from "./redux/store.ts";
+import store, { client } from "./redux/store";
 import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -26,3 +26,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </Provider>
   </React.StrictMode>
 );
+

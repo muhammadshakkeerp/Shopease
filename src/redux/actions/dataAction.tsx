d06f5531a08a07c4997e38683d8133be50eb7a13
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-02-08 21:11:33 +0530

    refactor: dataAction.tsx -> graphql changes , removed uncessary files

--- src/redux/actions/dataAction.tsx
+++ src/redux/actions/dataAction.tsx
@@ -1,73 +1,77 @@
-import { Dispatch, AnyAction } from "redux";
-
-import { gql } from "@apollo/client/core";
-import { client } from "../../apollo/client";
+import { Dispatch } from "redux";
 import { FetchProducts } from "../../types/globalTypes";
+import { gql, useQuery } from "@apollo/client";
+import { ThunkAction } from "redux-thunk";
+import { RootState } from "../store";
+import { AnyAction } from "@reduxjs/toolkit";
+import { client } from "../../apollo/client";
 
 // Action Types
 export const FETCH_DATA_REQUEST = "FETCH_DATA_REQUEST";
 export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
 export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
 
-// action types
-interface fetchDataRequesAction {
+interface fetchDataRequestAction {
   type: typeof FETCH_DATA_REQUEST;
 }
 interface fetchDataSuccessAction {
   type: typeof FETCH_DATA_SUCCESS;
-  loading: boolean;
   payload: FetchProducts[];
 }
 interface fatchDataFailureAction {
   type: typeof FETCH_DATA_FAILURE;
-  loading: boolean;
   payload: string;
 }
 export type DataFunctionsTypes =
-  | fetchDataRequesAction
+  | fetchDataRequestAction
   | fetchDataSuccessAction
   | fatchDataFailureAction;
 
-// Action Creators
-export const fetchDataRequest = () => ({
+export const fetchDataRequest = (): fetchDataRequestAction => ({
   type: FETCH_DATA_REQUEST,
 });
 
-export const fetchDataSuccess = (data: FetchProducts[]) => ({
+export const fetchDataSuccess = (data: FetchProducts[]): fetchDataSuccessAction => ({
   type: FETCH_DATA_SUCCESS,
   payload: data,
 });
 
-export const fetchDataFailure = (error: FetchProducts[]) => ({
+export const fetchDataFailure = (error: string): fatchDataFailureAction => ({
   type: FETCH_DATA_FAILURE,
   payload: error,
 });
 
-// Async Action to Fetch Data
-// ... (other imports and code)
-
-// Async Action to Fetch Data
 export const fetchData = () => {
   return async (dispatch: Dispatch<AnyAction>) => {
     try {
       dispatch(fetchDataRequest());
-      const result = await client.query({
-        query: gql`
-          query GetProducts {
-            products {
-              id
-              title
-              descriptionp
-              price
-              image
-            }
+      const GET_PRODUCTS = gql`
+      query {
+        products {
+          id
+          title 
+          price
+          description
+          category
+          image
+          rating {
+            rate
+            count
           }
-        `,
-      });
-      dispatch(fetchDataSuccess(result.data));
-    } catch (error:any) {
-      dispatch(fetchDataFailure(error.message)); // Dispatch error message on failure
+        }
+      }
+      `
+      const { data, error, loading } = await client.query({ query: GET_PRODUCTS })
+      dispatch(fetchDataSuccess(data.products));
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(fetchDataFailure(error.message));
+      } else {
+        dispatch(fetchDataFailure("An unknown error occurred"));
+      }
+
     }
   };
+
 };
 

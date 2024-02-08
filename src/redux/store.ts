2e75be31ca96daf6145f0b200725d38c0439d579
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-02-08 21:06:30 +0530

    refactor: store.ts -> veriable name changed, comment removed

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,9 @@ import { configureStore, Middleware } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import cartReducer from "./reducers/cartReducer";
 import searchReducer from "./reducers/searchReducer";
-// import authReducer, { AuthState } from "./reducers/authReducer";
 import dataReducer from "./reducers/dataReducer";
 import DarkReducer from "./reducers/DarkReducer";
+// import authReducer, { AuthState } from "./reducers/authReducer";
 
 export type CartState = ReturnType<typeof cartReducer>;
 export type DataState = ReturnType<typeof dataReducer>;
@@ -12,31 +12,26 @@ export type DarkState = ReturnType<typeof DarkReducer>;
 
 export type RootState = {
   cart: CartState;
-  data: DataState;
+  fetchData: DataState;
+  dark: DarkState;
   // auth: AuthState;
-  dark:DarkState
 };
 
 const rootReducer = combineReducers({
   cart: cartReducer,
-  data: dataReducer,
+  fetchData: dataReducer,
   search: searchReducer,
+  dark: DarkReducer,
   // auth: authReducer,
-  dark:DarkReducer
 });
 
-// Define any additional middleware here
-const additionalMiddleware: Middleware[] = [
-  // Add any custom middleware you want to include here
-  // For example: customLoggerMiddleware, customThunkMiddleware, etc.
-];
+const additionalMiddleware: Middleware[] = [];
 
-// Create the Redux store with middleware
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(additionalMiddleware),
-  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools in development
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;

9d73186b2b7b9e689c0fc903035d8e2f1986ae8b
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-02-17 16:03:58 +0530

    feature: add mubile Account sidebar

--- src/components/AccountSidebar.tsx
+++ src/components/AccountSidebar.tsx
@@ -6,8 +6,8 @@ export const AccountSidebar = () => {
   const [showActiveRoute, setShowActiveRoute] = useState("")
   console.log(showActiveRoute)
   return (
-    <ul>
-      {accountLinks?.map((item) => <Link key={item?.id} to={`/account/${item?.id}`}><li className={`xl:px-2 xl:py-4 text-start border font-semibold hover:bg-primary/50 ${item.id === showActiveRoute ? "bg-primary/50" : ""}`} onClick={() => setShowActiveRoute(item.id)}>{item.value}</li></Link>)}
+    <ul className="flex justify-center xl:justify-start xl:flex-col">
+      {accountLinks?.map((item) => <Link key={item?.id} to={`/account/${item?.id}`}><li className={`p-5 xl:px-2 xl:py-4 text-start border font-semibold hover:bg-primary/50 ${item.id === showActiveRoute ? "bg-primary/50" : ""}`} onClick={() => setShowActiveRoute(item.id)}>{item.value}</li></Link>)}
     </ul>
   )
 }
--- src/pages/Accout.tsx
+++ src/pages/Accout.tsx
@@ -3,13 +3,13 @@ import { AccountDetails, AccountSidebar, Login, Orders, Profile, Rewards, Wishli
 
 const Account = () => {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between flex-col xl:flex-row">
             {/* first section */}
-            <div className="hidden xl:block w-1/5">
+            <div className=" xl:block xl:w-1/5">
                 <AccountSidebar />
             </div>
             {/* second section */}
-            <div className="bg-primary w-full xl:w-4/5">
+            <div className="bg-primary w-full xl:xl:w-4/5">
                 <Routes>
                     <Route path="/account" element={<AccountDetails />} />
                     <Route path="/login" element={<Login />} />

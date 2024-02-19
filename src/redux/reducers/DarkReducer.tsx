823cbb3d2816391e98ffe19a79b118a6ca15cd00
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-02-19 19:03:48 +0530

    feature: implemented LocalStorage for Darkmode

--- src/redux/reducers/DarkReducer.tsx
+++ src/redux/reducers/DarkReducer.tsx
@@ -4,14 +4,17 @@ interface DarkModeProps {
     isEnabled: boolean
 }
 const initialState: DarkModeProps = {
-    isEnabled: false
+    isEnabled: localStorage.getItem('darkModeEnabled') === 'true'
 }
 
 const DarkReducer = (state = initialState, action: { type: string }): DarkModeProps => {
+    let   newEnabledState = false;
     switch (action.type) {
         case DARK__MODE_TOGGLE:
+             newEnabledState = !state.isEnabled;
+            localStorage.setItem("darkModeEnabled", newEnabledState.toString())
             return {
-                ...state, isEnabled: !state.isEnabled
+                ...state, isEnabled: newEnabledState
             }
         default:
             return state

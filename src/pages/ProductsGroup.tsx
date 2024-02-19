5d21d8395922697ed91e500e37cdc25d192cde34
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-02-19 17:07:47 +0530

    style:updated darkmode in productsGroup & products displaying

--- src/pages/ProductsGroup.tsx
+++ src/pages/ProductsGroup.tsx
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux"
 import { rowProductsData } from "../assets/globalUtlities"
 import { ProductCard, ProductFilterSiderbar, ProductGroupsTab } from "../paths"
+import { RootState } from "../redux/store"
 
 const ProductsGroup = () => {
+  const darkMode = useSelector((state: RootState) => state?.dark)
   return (
-    <div className="mt-1 md:mt-2">
+    <div className={`mt-1 md:mt-2 ${darkMode?.isEnabled ? "bg-darkModeBg" : "bg-white"} `}>
       <ProductGroupsTab />
       <section className="flex flex-col xl:flex-row mt-2">
         <ProductFilterSiderbar />
-        <div className="flex flex-wrap justify-between gap-2 bg-black ml-2" >
-          {rowProductsData?.map((_item, index) => <ProductCard product={undefined} index={index} refreshSite={false} />)}
+        <div className="flex flex-wrap gap-2  ml-2" >
+          {rowProductsData?.map((item, index) => <ProductCard product={item} index={index} refreshSite={false} />)}
         </div>
       </section>
     </div>

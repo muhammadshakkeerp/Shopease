e089a351dd4af004b43c9d38ee1f9f093c871724
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-09-12 18:29:42 +0530

    fetch products fucntion added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,16 @@
 import { ENDPOINTS } from "./endpoints";
+import { Product } from "./types";
 
 // https://claude.site/artifacts/fc5422b0-0df6-4dee-8102-74a083d0c50c
-export async function fetchProducts():{
-  
+export async function fetchProducts(): Promise<Product[]> {
+  const response = await fetch(ENDPOINTS.PRODUCTS);
+  return response.json();
 }
-export async function fetchProducts():{
-
-}
-export async function fetchProducts():{
-
-}
-export async function fetchProductsByCategory():{
-
-}
-export async function fetchUserCart():{
-
-}
-export async function fetchUsers():{
-
-}
+export async function fetchProduct(id:number):Promise<Product> {
+    const response = await fetch(ENDPOINTS.PRODUCT(id))
+    return response.json()
+};
+export async function fetchProducts(): {};
+export async function fetchProductsByCategory(): {};
+export async function fetchUserCart(): {};
+export async function fetchUsers(): {};

6ac386a3477ab5a598c14a87493f041fda745be8
MuhammadShakkeer2030 <simplefight90@gmail.com>
2024-09-12 08:39:46 +0530

    Initial implementation of API methods in api.ts: Added Added fetchProducts, fetchProductsByCategory, fetchUserCart, and fetchUsers leveraging ENDPOINTS for data fetching"

---
+++ src/api/api.ts
@@ -0,0 +1,21 @@
+import { ENDPOINTS } from "./endpoints";
+
+// https://claude.site/artifacts/fc5422b0-0df6-4dee-8102-74a083d0c50c
+export async function fetchProducts():{
+  
+}
+export async function fetchProducts():{
+
+}
+export async function fetchProducts():{
+
+}
+export async function fetchProductsByCategory():{
+
+}
+export async function fetchUserCart():{
+
+}
+export async function fetchUsers():{
+
+}
